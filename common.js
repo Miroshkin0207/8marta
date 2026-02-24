2dbcd42c5e4d037336886aa42e09bed67ce9d25f
Miroshkin0207 <vovamiroshkin2011@gmail.com>
2026-02-24 20:24:17 +0300

    Add files via upload

---
+++ common.js
@@ -0,0 +1,13 @@
+const script = document.createElement("script");
+script.src = "https://cdn.jsdelivr.net/gh/scottschiller/Snowstorm/snowstorm.js";
+document.body.appendChild(script);
+
+script.onload = () => {
+    snowStorm.snowCharacter = "🌸";
+    snowStorm.excludeMobile = false;
+    snowStor.zIndex = 2;
+    if (matchMedia("(max-width: 999px)").matches)
+    {
+            snowStorm.flakesMax = 5;
+    }
+}
